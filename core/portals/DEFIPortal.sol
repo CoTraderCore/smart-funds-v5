13598f759bc19439f4af4ec9312bfddda529680b
Ruslan <ruslan777mirov@gmail.com>
2020-02-09 13:03:00 +0200

    Update DEFIPortal

--- core/portals/DEFIPortal.sol
+++ core/portals/DEFIPortal.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.4.24;
 
 import "../../zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../../compound/CEther.sol";
+import "../../compound/CToken.sol";
 
 contract DEFIPortal {
   CEther public cEther;
+  CToken cToken;
   address public Comptroller;
 
   enum CompoundAction { Mint, Withdraw }
@@ -28,32 +31,61 @@ contract DEFIPortal {
   /**
   * @dev Facilitates for borrow, withdraw, liquidate or mint from Compound
   *
-  * @param _source                  ERC20 token to convert from or Compoud token
-  * @param _sourceAmount            Amount to convert from (in _source token)
+  * @param _cAddress                 compound CToken address
+  * @param _ercAddress               erc20 token address, for eth operation just set 0x address
+  * @param _sourceAmount             amount to convert from (in _source token)
   * @return The amount of _destination received from the trade
   */
-  function compound(ERC20 _source, uint256 _sourceAmount, uint action)
+  function compound(
+    address _cAddress,
+    ERC20 _ercAddress,
+    uint256 _sourceAmount,
+    uint _action
+  )
   external
   payable
-  returns(uint256)
-  {
-   if(action == uint(CompoundAction.Mint)){
-     if(address(_source) != address(cEther)){
-       // approve erc20
-       _transferFromSenderAndApproveTo(_source, _sourceAmount, Comptroller);
-       cToken.mint(_sourceAmount);
-     }else{
+  returns(uint256 returnAmount){
+   // Action Mint
+   if(_action == uint(CompoundAction.Mint)){
+     if(_cAddress == address(cEther)){
        require(msg.value == _sourceAmount);
        cEther.mint.value(_sourceAmount)();
+
+       // transfer CEther to sender
+       returnAmount = ERC20(cEther).balanceOf(address(this));
+       ERC20(cEther).transfer(msg.sender, returnAmount);
+     }else{
+       // approve erc20
+       _transferFromSenderAndApproveTo(_ercAddress, _sourceAmount, Comptroller);
+       cToken = CToken(_cAddress);
+
+       // mint
+       cToken.mint(_sourceAmount);
+
+       // transfer CToken to sender
+       returnAmount = ERC20(cToken).balanceOf(address(this));
+       ERC20(cToken).transfer(msg.sender, returnAmount);
      }
     }
-    else if(action == uint(CompoundAction.Withdraw)){
-      if(address(_source) != address(cEther)){
-        // approve cTokens
-        _transferFromSenderAndApproveTo(_source, _sourceAmount, Comptroller);
-        cToken.redeemUnderlying(_sourceAmount);
+    // Action Withdraw
+    else if(_action == uint(CompoundAction.Withdraw)){
+      if(_cAddress == address(cEther)){
+         // approve CEther not erc20!
+         _transferFromSenderAndApproveTo(ERC20(_cAddress), _sourceAmount, Comptroller);
+         cEther.redeemUnderlying(_sourceAmount);
+
+         // transfer ETH to sender
+         returnAmount = address(this).balance;
+         (msg.sender).transfer(returnAmount);
+
        }else{
-        cEther.redeemUnderlying(_sourceAmount);
+         _transferFromSenderAndApproveTo(ERC20(_cAddress), _sourceAmount, Comptroller);
+         cToken = CToken(_cAddress);
+         cToken.redeemUnderlying(_sourceAmount);
+
+         // transfer ERC20 to sender 
+         returnAmount = _ercAddress.balanceOf(address(this));
+         _ercAddress.transfer(msg.sender, returnAmount);
       }
     }
     else{
